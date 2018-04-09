bb3383d6d9ff198b328d221b58ef64265f367f58
will <williamparks94@gmail.com>
2018-04-09 11:25:28 -0400

    need to start the Settings fragment on clicking the settings icon

--- app/src/main/java/com/bandw/sparks/fragments/SavedFragment.java
+++ app/src/main/java/com/bandw/sparks/fragments/SavedFragment.java
@@ -4,6 +4,8 @@ package com.bandw.sparks.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,18 +16,32 @@ import com.bandw.sparks.R;
  * A simple {@link Fragment} subclass.
  */
 public class SavedFragment extends Fragment {
-
+    private static final String TAg = "SavedFragment";
 
     public SavedFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        // Orientation change handling
+        setRetainInstance(true);
+        // Toolbar Menu
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_saved, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_saved, menu);
     }
 
 }
